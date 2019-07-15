07f0dc6d8a3a607d9ec4d61ae277feb68903bb60
Casey Dwyer <caseydwyer@gmail.com>
2019-07-15 14:12:00 -0700

    Update.

--- io/ioutil/ioutil.c
+++ io/ioutil/ioutil.c
@@ -1,37 +1,52 @@
 #include "io/ioutil/ioutil.h"
 
 extern os_FileInfo **ioutil_read_dir(const char *name, error_t **error) {
+    os_FileInfo **info = NULL;
     error_t *err = NULL;
     os_File *file = os_openDir(name, &err);
     if (err != NULL) {
-        error_move(err, error);
-        return NULL;
+        goto end;
     }
-    os_FileInfo **info = os_readdir(file, &err);
+    info = os_readdir(file, &err);
     if (err != NULL) {
+        goto end;
+    }
+end:
+    if (file != NULL) {
         os_close(file, NULL);
-        error_move(err, error);
-        return NULL;
     }
-    os_close(file, &err);
     if (err != NULL) {
         error_move(err, error);
-        return NULL;
     }
     return info;
 }
 
 extern char *ioutil_read_file(const char *name, error_t **error) {
-    os_File *file = os_open(name, error);
-    strings_Builder builder = {};
+    char *ret = NULL;
+    error_t *err = NULL;
+    os_File *file = os_open(name, &err);
+    if (err != NULL) {
+        goto end;
+    }
+    strings_Builder b = {};
     for (;;) {
         char buf[BUFSIZ];
-        int n = os_read(file, buf, BUFSIZ, error);
-        strings_Builder_write(&builder, buf, n, error);
+        int n = os_read(file, buf, BUFSIZ, &err);
+        if (err != NULL) {
+            goto end;
+        }
+        strings_Builder_write(&b, buf, n, NULL); // ignore error
         if (n < BUFSIZ) {
             break;
         }
     }
-    os_close(file, error);
-    return strings_Builder_string(&builder);
+    ret = strings_Builder_string(&b);
+end:
+    if (file != NULL) {
+        os_close(file, &err);
+    }
+    if (err != NULL) {
+        error_move(err, error);
+    }
+    return ret;
 }
