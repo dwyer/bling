357709e3b21163115970c3606512f9caf17a7dc8
Casey Dwyer <caseydwyer@gmail.com>
2019-08-22 22:59:51 -0700

    bootstrap: Cleanup.

--- bootstrap/bootstrap.c
+++ bootstrap/bootstrap.c
@@ -1,8 +1,10 @@
 #include "bootstrap.h"
 
-#include <execinfo.h>
+#include <execinfo.h> // backtrace, etc.
 #include <stdarg.h>
-#include <stdio.h>
+#include <stdio.h> // fprintf, BUFSIZ
+#include <stdlib.h> // exit
+#include <unistd.h> // STDERR_FILENO
 
 extern void print(const char *s) {
     fprintf(stderr, "%s\n", s);
@@ -10,10 +12,9 @@ extern void print(const char *s) {
 
 extern void panic(const char *s) {
     print(s);
-    void *buf[1024];
-    int n = backtrace(buf, 1024);
-    int fd = 2; //stderr
-    backtrace_symbols_fd(buf, n, fd);
+    void *buf[BUFSIZ];
+    int n = backtrace(buf, BUFSIZ);
+    backtrace_symbols_fd(buf, n, STDERR_FILENO);
     exit(1);
 }
 
--- bootstrap/bootstrap.h
+++ bootstrap/bootstrap.h
@@ -1,23 +1,35 @@
 #ifndef BLING_BOOTSTRAP_H
 #define BLING_BOOTSTRAP_H
 
-#include <stdbool.h>
-#include <stddef.h>
 #include <stdint.h>
-#include <stdlib.h>
-#include <strings.h>
 
 #define import(_) /* noop in C */
 #define package(_) /* noop in C */
-#define esc(x) ({typeof(x) $0 = (x); memcpy(malloc(sizeof $0), &$0, sizeof $0);})
+#define esc(x) ({ \
+        void *memcpy(void *, void const*, size_t); \
+        typeof(x) $0 = (x); \
+        memcpy(malloc(sizeof $0), &$0, sizeof $0); \
+        })
+#define assert(x) do { if (!(x)) panic("assert failed: " # x); } while (0)
 
 #define fallthrough /**/
 #define typ typedef
 
-#define assert(x) do { if (!(x)) panic("assert failed: " # x); } while (0)
+#define NULL ((void*)0)
 
+#define false (0)
+#define true (!false)
+
+typedef unsigned long int size_t;
+typedef _Bool bool;
+
+extern void free(void *);
+extern void *malloc(size_t);
+
+extern char *strdup(char const *);
+extern size_t strlen(char const *);
 extern void print(const char *s);
-extern void panic(const char *s);
+extern void panic(const char *s) __attribute__((noreturn));
 extern bool streq(const char *a, const char *b);
 
 #endif
